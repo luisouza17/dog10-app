afb9d95c6651283e7c6d97601f7d3bec761bb439
luisouza17 <luis.souza.dell@hotmail.com>
2019-08-10 20:30:25 -0300

    first commit

---
+++ src/app/domain/endereco.ts
@@ -0,0 +1,12 @@
+export default class Endereco {
+    rua: String;
+    cep: String;
+    numero: String;
+    bairro: String;
+    cidade: String;
+    estado: String;
+  
+    salvar() {
+      localStorage.setItem((Math.random()* 100).toString(), JSON.stringify(this))
+    }
+  } 
--- src/app/finalizar-pedido/finalizar-pedido.page.ts
+++ src/app/finalizar-pedido/finalizar-pedido.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NavController, AlertController } from '@ionic/angular';
+import Endereco from '../domain/Endereco';
 
 @Component({
   selector: 'app-finalizar-pedido',
@@ -7,9 +9,68 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FinalizarPedidoPage implements OnInit {
 
-  constructor() { }
+  constructor(private Alert:AlertController,private nav:NavController) { }
 
   ngOnInit() {
+  }
+
+  buscar(cep) {
+    const cepString = cep.el.value
+    if(cepString === '' || cepString.length !== 8 || !cepString.match(/^\d+$/g)) {
+      console.log("CEP inválido");
+    }else {
+      console.log("CEP válido")
+      let retorno = fetch('https://viacep.com.br/ws/' + cepString + '/json')
+      console.log("Enviando requisição...")
+      retorno.then(dados => {
+        return dados.json()
+      }).then(endereco => {
+        if(endereco.erro) {
+          console.error("CEP Inexistente")
+        }else {
+          this.Alert.create({
+            header: "Seu endereço está correto:",
+            subHeader: `${endereco.logradouro} , ${endereco.bairro},${endereco.localidade} - ${endereco.uf.toUpperCase()}`,
+            buttons:[{
+              text:'Não'
+            },{
+                text:'Sim',
+                handler:() =>{
+                  this.Alert.dismiss()
+                  this.Alert.create({
+                    header:"Qual o seu número?",
+                    inputs:[{
+                      name:"Numero",
+                      type:"number"
+                    }],
+                    buttons:[{
+                      text:'Cancelar',
+                    },{
+                      text:'Salvar',
+                      handler: (dados)=>{
+                        let tempend = new Endereco()
+                        tempend.bairro= endereco.bairro;
+                        tempend.cidade=endereco.localidade;
+                        tempend.estado=endereco.uf;
+                        tempend.numero= dados.numero;
+                        tempend.rua=endereco.logradouro;
+                        tempend.cep = endereco.cep;
+                        tempend.salvar()
+                        this.nav.back()
+                      }
+                    }]
+                  }).then(alert=>{
+                    alert.present();
+                  })
+                }
+              }
+          ]
+          }).then(alert => {
+            alert.present()
+          })
+        }
+      })
+    }
   }
 
 }
